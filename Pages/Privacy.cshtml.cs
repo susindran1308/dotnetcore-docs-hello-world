589423865d812b1b4c0523088432143ab5de9ab3
susindran1308 <40034152+susindran1308@users.noreply.github.com>
2023-06-23 11:34:05 +0530

    Updating more error codes

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -15,7 +15,7 @@ public class PrivacyModel : PageModel
     public IActionResult OnGet()
     {
         // Define an array or list of valid HTTP error codes
-        int[] validErrorCodes = { 400, 401, 403, 404, 500 };
+        int[] validErrorCodes = { 200, 400, 401, 403, 404, 406, 500, 501, 502, 200, 200, 200, 200, 200, 200 };
 
         // Generate a random index to select a random error code from the array/list
         Random random = new Random();
