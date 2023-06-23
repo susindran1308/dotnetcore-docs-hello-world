45a9bc9f488b3fd2400e192b5b05a57db95cb667
susindran1308 <40034152+susindran1308@users.noreply.github.com>
2023-06-23 11:26:42 +0530

    error code handling for privacy url

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -14,6 +14,18 @@ public class PrivacyModel : PageModel
 
     public void OnGet()
     {
+        // Define an array or list of valid HTTP error codes
+        int[] validErrorCodes = { 400, 401, 403, 404, 500 };
+
+        // Generate a random index to select a random error code from the array/list
+        Random random = new Random();
+        int randomIndex = random.Next(validErrorCodes.Length);
+
+        // Get the random error code
+        int randomErrorCode = validErrorCodes[randomIndex];
+
+        // Return the error response with the random error code
+        return StatusCode(randomErrorCode);
     }
 }
 
